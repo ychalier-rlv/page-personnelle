65f9522f6ed02b24a48d82d12f42c2617235187f
Yohan Chalier <yohan@chalier.fr>
2023-01-04 15:36:11 +0100

    added page-personnelle

---
+++ page-personnelle/README.md
@@ -0,0 +1,9 @@
+# Page Personnelle
+
+En guise d'initiation au développement web, on crée une page de présentation pour un site web personnel.
+
+Pour générer du texte factice, on peut utiliser [Lorem Ipsum](https://fr.lipsum.com/feed/html).
+
+Utiliser une police au choix depuis [Google Fonts](https://fonts.google.com/).
+
+Pour créer l'avatar servant d'animation, utiliser le site [Avataaars Generator](https://getavataaars.com/). Pour que le rendu de l'animation des yeux fonctionne bien, utiliser les yeux *Surprised*.
---
+++ page-personnelle/master.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", () => {
+
+    function setupEye(eyeId) {
+        const element = document.getElementById(eyeId);
+        const boundingClientRect = element.getBoundingClientRect();
+        return {
+            element: element,
+            originAbsolute: {
+                x: boundingClientRect.x + boundingClientRect.width / 2,
+                y: boundingClientRect.y + boundingClientRect.height / 2
+            },
+            originRelative: {
+                x: parseFloat(element.getAttribute("cx")),
+                y: parseFloat(element.getAttribute("cy")),
+            }
+        }
+    }
+
+    const eyes = [setupEye("Eye1"), setupEye("Eye2")];
+    const radius = 5;
+
+    window.addEventListener("mousemove", (event) => {
+        eyes.forEach(eye => {
+            const dx = eye.originAbsolute.x - event.clientX;
+            const dy = eye.originAbsolute.y - event.clientY;
+            const angle = Math.atan2(dy, dx);
+            eye.element.setAttribute("cx", eye.originRelative.x - radius * Math.cos(angle));
+            eye.element.setAttribute("cy", eye.originRelative.y - radius * Math.sin(angle));
+        });
+    });
+
+
+});
